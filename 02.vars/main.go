3e57e4b881d134f679179853d750e9fe8713e497
rakan <1769434268@qq.com>
2022-12-07 00:15:25 +0800

    变量1

--- 02.vars/main.go
+++ 02.vars/main.go
@@ -1,8 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 	var hello string = "hello,golang"
 	var world = "world"
 	println(hello, world)
+	小数 := 1.234
+	fmt.Println(小数)
+
+	var int3, int4 uint = 33, 44
+	fmt.Println(int3 * int4)
+
+	var ho, ver = 3, 4
+	var sc = ho * ver
+	fmt.Println(ho * ver)
+	fmt.Println(sc)
 
 }
