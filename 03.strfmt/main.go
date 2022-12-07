cc66c80327009ecfde267b1acd165491070fdc3c
rakan <1769434268@qq.com>
2022-12-08 00:36:35 +0800

    字符串输出

---
+++ 03.strfmt/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("中文，abc,1234,♥")
+	fmt.Printf("我的名字是%s\n", "kk")
+	fmt.Printf("我的名字是%q\n", "kk")
+	fmt.Printf("我的名字是%x\n", "kk")
+	fmt.Printf("我的名字是%X\n", "kk")
+
+}
