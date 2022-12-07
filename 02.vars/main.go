b9444f50412c5cb4023d1f27bec58ae0ef3bb6ec
rakan <1769434268@qq.com>
2022-12-07 23:25:39 +0800

    变量不能做类型转换

--- 02.vars/main.go
+++ 02.vars/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"reflect"
+)
 
 func main() {
 	var hello string = "hello,golang"
@@ -17,4 +21,17 @@ func main() {
 	fmt.Println(ho * ver)
 	fmt.Println(sc)
 
+	a1 := "hello"
+	fmt.Println(reflect.TypeOf(a1))
+	a2 := 3
+	fmt.Println(reflect.TypeOf(a2))
+	a3 := 3.0
+	fmt.Println(reflect.TypeOf(a3))
+	a4 := &a3
+	fmt.Println(reflect.TypeOf(a4))
+
+	var int6 uint = math.MaxUint64
+	fmt.Println(int6)
+	var int7 int = int(int6)
+	fmt.Println(int7)
 }
