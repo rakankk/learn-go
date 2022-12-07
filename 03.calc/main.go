d3b6c5b2ba4a27235753fcbcec036bfefe0dfe46
rakan <1769434268@qq.com>
2022-12-08 00:28:57 +0800

    比较运算符

--- 03.calc/main.go
+++ 03.calc/main.go
@@ -1,1 +1,14 @@
-package _3_calc
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int8 = 30, 10
+	fmt.Println("加法：a + b = ", a+b)
+	fmt.Println("减法：a - b = ", a-b)
+	fmt.Println("乘法：a * b = ", a*b)
+	fmt.Println("除法：a / b = ", a/b)
+	fmt.Println("取余：a % b = ", a%b)
+
+	fmt.Println(true && false == false)
+}
