14c16aa948ee215a1a186e829b8e235c35dac962
rakan <1769434268@qq.com>
2022-12-14 23:20:25 +0800

    并入其他分支继续执行

--- 04.switch-case/main.go
+++ 04.switch-case/main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var money int = 200
+	var money int = 20
 	var busy bool
 	// var busy bool = true
 	switch money {
 	case 20:
 		fmt.Println("点个外卖")
+		fallthrough
 	case 200:
 		fmt.Println("下个馆子")
 		// 如果忙，不吃了，否则再吃点零食
