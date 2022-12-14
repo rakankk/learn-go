e4e8aaae3479adaf3b5c6f4dd7f10312c97e8abd
rakan <1769434268@qq.com>
2022-12-14 23:05:39 +0800

    if条件执行顺序

--- 04.ifelseif/main.go
+++ 04.ifelseif/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("test")
+	var rmb int = 21
+	if rmb <= 20 {
+		fmt.Println("点个外卖")
+	} else if rmb <= 200 {
+		// 如果有不超过20块钱，点个外卖
+		fmt.Println("下个馆子")
+	} else if rmb <= 2000 {
+		// 如果不超过2000，去新马泰
+		fmt.Println("去米其林")
+	} else if rmb <= 20000 {
+		fmt.Println("去新马泰")
+
+	} else {
+		fmt.Println("容我想想")
+	}
+	// 如果不超过200，下个馆子
+	// 如果不超过2000，去米其林
+	// 如果不超过20000，去新马泰
+	// 如果再多，容我想想
+	// 在整个函数里面，程序从上到下依次执行，满足条件后就会执行，后面执行后跳出if直接end
 }
